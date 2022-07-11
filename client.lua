4a84aa64b1df0d1afe30bee6851fe51fe9d07dbe
btwlouis <louischmidt0402@gmail.com>
2022-07-11 02:39:26 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -9,3 +9,10 @@ AddEventHandler('notifications', function(color, title, message)
     
 	PlaySoundFrontend(-1, "ATM_WINDOW", "HUD_FRONTEND_DEFAULT_SOUNDSET", 1)
 end)
+
+RegisterCommand("test_notify", function()
+    TriggerEvent('notifications', "info", "Awesome title", "Awesome message")
+    TriggerEvent('notifications', "orange", "Awesome title", "Awesome message")
+    TriggerEvent('notifications', "grey", "Awesome title", "Awesome message")
+    TriggerEvent('notifications', "green", "Awesome title", "Awesome message")
+end, false)
